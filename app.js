2779d30be82c44a9b41a51047643fd9f4e52ec54
jihadcodes <jihad.dev.pro@gmail.com>
2026-03-27 11:31:53 +0600

    typescript data types

---
+++ app.js
@@ -0,0 +1,33 @@
+"use strict";
+// Basic TypeScript 
+// Primitive data types (number, string, boolean, null, undefined, symbol, bigint)
+// Arrays, 
+// tuples, 
+// enums, 
+// any, void, never, unknown
+Object.defineProperty(exports, "__esModule", { value: true });
+// let a: number = 10;
+// let b: string = "Hello, TypeScript!";
+// let c: boolean = true;
+// let d: null = null;
+// let e: undefined = undefined;
+// let f: symbol = Symbol("unique");
+// let g: bigint = 9007199254740991n;
+// tuples, 
+let tuple = [3, "Rohan"];
+let tuple2 = [1, "Hello", true];
+// enums, (by default, enums are number-based, starting at 0. You can also assign specific values to enum members.) "enumerations"
+// enum Color {
+//     Red,    // 0
+//     Green,  // 1
+//     Blue    // 2
+// }
+// enum userRole {
+//     ADMIN = "admin",
+//     USER = "user",
+//     GUEST = "guest"
+// }
+// userRole.ADMIN; // "admin"
+// userRole.USER; // "user"
+// any, void, never, unknown,null, undefined
+//# sourceMappingURL=app.js.map
